eadb7b6f4d77298a50be6fb29d9311254416eb97
Brandon Ray <brandonray1225@gmail.com>
2019-02-18 00:33:12 -0600

    Sort temp spread array and return smallest value

--- temp_spread.rb
+++ temp_spread.rb
@@ -1,13 +1,17 @@
-raw_data = File.readlines('weather.dat').map { |line| line.split(' ') }
-raw_data.shift(2)
+def small_spread
+  raw_data = File.readlines('weather.dat').map { |line| line.split(' ') }
+  raw_data.shift(2)
 
-temp_spread = []
+  temp_spread = []
 
-index = 0
-raw_data.length.times do
-  spread = raw_data[index][1].to_i - raw_data[index][2].to_i
-  temp_spread << spread
-  index = index + 1
+  index = 0
+  raw_data.length.times do
+    spread = raw_data[index][1].to_i - raw_data[index][2].to_i
+    temp_spread << spread
+    index = index + 1
+  end
+  temp_spread = temp_spread.sort
+  return "The smallest temperature spread was #{temp_spread[0]}."
 end
 
-p temp_spread
+p small_spread
