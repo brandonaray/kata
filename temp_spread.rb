3cdc1c051a297f5d97fe07a1ec7b0f6c76d8ec12
Brandon Ray <brandonray1225@gmail.com>
2019-02-18 00:24:34 -0600

    Create loop to calculate daily temp spread and add to temp_spread array

--- temp_spread.rb
+++ temp_spread.rb
@@ -1,7 +1,13 @@
 raw_data = File.readlines('weather.dat').map { |line| line.split(' ') }
 raw_data.shift(2)
 
-index = 0
 temp_spread = []
-spread = raw_data[2][1].to_i - raw_data[2][2].to_i
-temp_spread << spread
+
+index = 0
+raw_data.length.times do
+  spread = raw_data[index][1].to_i - raw_data[index][2].to_i
+  temp_spread << spread
+  index = index + 1
+end
+
+p temp_spread
