131b32cd2146afaa0c8f8a8464e3740b1059aec3
Brandon Ray <brandonray1225@gmail.com>
2019-02-18 00:41:41 -0600

    Remove month data from array; add both day and temp spread to temp_spread array

--- temp_spread.rb
+++ temp_spread.rb
@@ -1,17 +1,17 @@
 def small_spread
   raw_data = File.readlines('weather.dat').map { |line| line.split(' ') }
   raw_data.shift(2)
+  raw_data.pop
 
   temp_spread = []
 
   index = 0
   raw_data.length.times do
     spread = raw_data[index][1].to_i - raw_data[index][2].to_i
-    temp_spread << spread
+    temp_spread << [raw_data[index][0], spread]
     index = index + 1
   end
-  temp_spread = temp_spread.sort
-  return "The smallest temperature spread was #{temp_spread[0]}."
+  return temp_spread
 end
 
 p small_spread
