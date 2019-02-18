06418421509e3feb863421942a60d9057c37451b
Brandon Ray <brandonray1225@gmail.com>
2019-02-18 00:20:01 -0600

    Remove first two lines of spreadsheet data

--- temp_spread.rb
+++ temp_spread.rb
@@ -1,3 +1,7 @@
 raw_data = File.readlines('weather.dat').map { |line| line.split(' ') }
+raw_data.shift(2)
 
-p raw_data
+index = 0
+temp_spread = []
+spread = raw_data[2][1].to_i - raw_data[2][2].to_i
+temp_spread << spread
